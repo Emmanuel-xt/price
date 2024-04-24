ca1316320ae25211eaf198b009d1033fdd1a725b
Emmanuel-xt <fattmane5@gmail.com>
2024-04-24 21:33:01 +0100

    layout

---
+++ app/(root)/page.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div className='font-bold  text-light-1'>Home hello world</div>
+  )
+}
+
+export default Home
---
+++ constants/index.tsx
@@ -0,0 +1,69 @@
+export const sidebarLinks = [
+    {
+      imgURL: "/assets/home.svg",
+      route: "/",
+      label: "Home",
+    },
+    {
+      imgURL: "/assets/search.svg",
+      route: "/search",
+      label: "Search",
+    },
+    {
+      imgURL: "/assets/heart.svg",
+      route: "/activity",
+      label: "Activity",
+    },
+    {
+      imgURL: "/assets/create.svg",
+      route: "/create-thread",
+      label: "Create Thread",
+    },
+    // {
+    //   imgURL: "/assets/community.svg",
+    //   route: "/communities",
+    //   label: "Communities",
+    // },
+    {
+      imgURL: "/assets/user.svg",
+      route: "/profile",
+      label: "Profile",
+    },
+  ];
+  
+  export const profileTabs = [
+    { value: "threads", label: "Threads", icon: "/assets/reply.svg" },
+    { value: "replies", label: "Replies", icon: "/assets/members.svg" },
+    { value: "tagged", label: "Tagged", icon: "/assets/tag.svg" },
+  ];
+  
+  export const communityTabs = [
+    { value: "threads", label: "Threads", icon: "/assets/reply.svg" },
+    { value: "members", label: "Members", icon: "/assets/members.svg" },
+    { value: "requests", label: "Requests", icon: "/assets/request.svg" },
+  ];
+
+  export const studentItems = [
+    { name: "Notebook", price: 2.50 },
+    { name: "Pens (Pack of 5)", price: 3.99 },
+    { name: "Textbook", price: 20.00 },
+    { name: "Laptop", price: 800.00 },
+    { name: "Backpack", price: 30.00 },
+    { name: "Water Bottle", price: 5.00 },
+    { name: "Calculator", price: 10.00 },
+    { name: "USB Flash Drive", price: 8.00 },
+    { name: "Headphones", price: 15.00 },
+    { name: "T-shirts", price: 12.99 },
+    { name: "Jeans", price: 25.00 },
+    { name: "Sneakers", price: 40.00 },
+    { name: "Toothpaste", price: 2.00 },
+    { name: "Shampoo", price: 5.50 },
+    { name: "Soap", price: 1.50 },
+    { name: "Instant Noodles", price: 0.99 },
+    { name: "Rice (5kg)", price: 10.00 },
+    { name: "Canned Beans", price: 1.50 },
+    { name: "Bread", price: 2.50 },
+    { name: "Milk (1 liter)", price: 3.00 }
+  ];
+  
+  
