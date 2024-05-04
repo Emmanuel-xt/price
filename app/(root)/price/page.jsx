7c0279f17807002f8659144e9dbad9d9f4387a04
Emmanuel-xt <fattmane5@gmail.com>
2024-05-05 00:10:54 +0100

    fixed caching issues temp

--- app/(root)/price/page.jsx
+++ app/(root)/price/page.jsx
@@ -1,20 +1,23 @@
-
 import PriceTabs from "@/components/PriceTabs";
 import { getAllItems } from "@/lib/actions/price.actions";
 import Image from "next/image";
 
 const page = async ({ searchParams }) => {
   // const [studentItems, setStudentItems] = useState();
+  const items = await getAllItems();
   try {
-    const items = await getAllItems();
     console.log("items =", items);
   } catch (error) {
     console.error("Error fetching items:", error);
   }
-  
+
   return (
     <div>
-    <PriceTabs studentItems={items} searchParams={searchParams} />
+      {items.length === 0 ? (
+        <p className="">Nothing FOund</p>
+      ) : (
+        <PriceTabs studentItems={items} searchParams={searchParams} />
+      )}
     </div>
   );
 };
